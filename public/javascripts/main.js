37c177ffb63660d6bc70c9a18108b5d69e3aa745
Prekshya Dawadi <prekshya019@gmail.com>
2023-02-11 20:40:21 +0545

    Username works and general backend works, todo: online users list display and front end

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,46 +6,51 @@
 
   const socket = io();
 
-  // Returns the url of the current page
-  var url = window.location.href.toString();
-  //Returns the room number
-  Room = url.replace("http://localhost:3000/chat/:", "");
+  // Returns the url of the current page. 
+var url = window.location.href.toString();
+//Returns the room number
+Room = url.replace('http://localhost:3000/chat/:', ''); 
+console.log(Room);
 
-  //Submitting the message
-  chatForm.addEventListener("submit", (e) => {
-    // when we submit a form, it automatically submits to a
-    //file, to prevent that from happening, we use prevent default
+
+// Message submit
+chatForm.addEventListener('submit', (e)=>{
+    // when we submit a form, it automatically submits to a file
+    // to prevent that from happening, we use prevent default
     e.preventDefault();
 
-    // Get the message text
-    var msg = e.target.elements.msg.value;
+    // Get message text
+    const msg = e.target.elements.msg.value;
 
-    // emit message to server
+    // Emit message to server
     socket.emit('chatMessage', msg);
 
-    //clear inputs
+    //Clear inputs
     e.target.elements.msg.value = '';
     e.target.elements.msg.focus();
-  });
+});
 
-  //Message from server
-  socket.on('message', message => {
+// Message from server
+socket.on('message',message=>{
     outputMessage(message);
     console.log(message);
     //scroll down
     chatMessages.scrollTop = chatMessages.scrollHeight;
-  });
+ 
+});
 
-  // Output message to DOM
-  function outputMessage(message) {
+
+// Output message to DOM
+function outputMessage(message){
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML = `<p class="meta>${message.username} <span>${message.time}</span></p>
-    <p class = 'text'>
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
         ${message.text}
     </p>`;
     document.querySelector('.chat-messages').appendChild(div);
-  }
+}
 
-  //Rooms
-  socket.emit("joinRoom", Room);
+//Rooms
+
+socket.emit('joinRoom', Room);  
